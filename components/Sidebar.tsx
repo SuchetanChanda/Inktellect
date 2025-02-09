2e12e7c299c0e08b1be2c66ff20e2578f9c11059
SuchetanChanda <chanda.suchetan@gmail.com>
2025-02-10 00:02:47 +0530

    implemented new chat functionality

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,15 +4,26 @@ import { PlusIcon } from "@radix-ui/react-icons";
 import { useRouter } from "next/navigation";
 import { use } from "react";
 import { Button } from "./ui/button";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 function Sidebar() {
   const router = useRouter();
   const { closeMobileNav, isMobileNavOpen } = use(NavigationContext);
 
+  const createChat = useMutation(api.chats.createChat);
   // const handleClick = () => {
   //     router.push(`/dashboard/chat/${chat._id}`);
   //     closeMobileNav();
   //   };
+
+  const handleNewChat = async () => {
+    const chatID = await createChat({ title: "New Chat" });
+    if (chatID) {
+      router.push(`/dashboard/chat/${chatID}`);
+      closeMobileNav();
+    }
+  };
   return (
     <>
       {/* Background Overlay for mobile */}
@@ -31,7 +42,7 @@ function Sidebar() {
       >
         <div className="p-4 border-b border-gray-200/50">
           <Button
-            // onClick={handleNewChat}
+            onClick={handleNewChat}
             className="w-full bg-white hover:bg-gray-50 text-gray-700 border border-gray-200/50 shadow-sm hover:shadow transition-all duration-200"
           >
             <PlusIcon className="mr-2 h-4 w-4" /> New Chat
